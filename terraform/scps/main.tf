5d7a5bde473014283acdab185cdb4bd98c0bcee2
Wilfred Ballo <wilfredballo@macbooks-MacBook-Pro.local>
2026-04-23 12:33:35 +0100

    added full SCP governance layer (cloudtrail + regions)

--- terraform/scps/main.tf
+++ terraform/scps/main.tf
@@ -9,9 +9,9 @@ resource "aws_organizations_policy" "deny_leave_org" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "DenyLeaveOrganization"
-        Effect   = "Deny"
-        Action   = [
+        Sid    = "DenyLeaveOrganization"
+        Effect = "Deny"
+        Action = [
           "organizations:LeaveOrganization"
         ]
         Resource = "*"
@@ -22,5 +22,68 @@ resource "aws_organizations_policy" "deny_leave_org" {
 
 resource "aws_organizations_policy_attachment" "deny_leave_org_to_sandbox" {
   policy_id = aws_organizations_policy.deny_leave_org.id
+  target_id = "ou-adtv-j1z2kcwv"
+}
+
+resource "aws_organizations_policy" "deny_disable_cloudtrail" {
+  name        = "DenyDisableCloudTrail"
+  description = "Prevents disabling or deleting CloudTrail"
+  type        = "SERVICE_CONTROL_POLICY"
+
+  content = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "DenyDisableCloudTrail"
+        Effect = "Deny"
+        Action = [
+          "cloudtrail:StopLogging",
+          "cloudtrail:DeleteTrail"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_organizations_policy_attachment" "deny_disable_cloudtrail_to_sandbox" {
+  policy_id = aws_organizations_policy.deny_disable_cloudtrail.id
+  target_id = "ou-adtv-j1z2kcwv"
+}
+
+resource "aws_organizations_policy" "restrict_regions" {
+  name        = "RestrictRegions"
+  description = "Allow only approved AWS regions"
+  type        = "SERVICE_CONTROL_POLICY"
+
+  content = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "DenyUnapprovedRegions"
+        Effect = "Deny"
+        NotAction = [
+          "iam:*",
+          "organizations:*",
+          "route53:*",
+          "cloudfront:*",
+          "support:*"
+        ]
+        Resource = "*"
+        Condition = {
+          StringNotEquals = {
+            "aws:RequestedRegion" = [
+              "eu-west-2",
+              "eu-west-1"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_organizations_policy_attachment" "restrict_regions_to_sandbox" {
+  policy_id = aws_organizations_policy.restrict_regions.id
   target_id = "ou-adtv-j1z2kcwv"
 }
